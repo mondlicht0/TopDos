67d3a506abbdd56f5f4bc203dda608d3f7481a0f
Damir <139867315+mondlicht0@users.noreply.github.com>
2024-04-19 02:29:42 +0500

    unitask

--- Assets/Scripts/Runtime/Animations/OnFinish.cs
+++ Assets/Scripts/Runtime/Animations/OnFinish.cs
@@ -1,14 +1,15 @@
-using TopDos.Animations;
+using System;
 using UnityEngine;
+using Cysharp.Threading.Tasks;
 
 public class OnFinish : StateMachineBehaviour
 {
     [SerializeField] private float _crossfade;
     
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    public override async void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        await UniTask.Delay(TimeSpan.FromSeconds(stateInfo.length));
         animator.CrossFade("Empty", _crossfade, 1);
-        
     }
 }
